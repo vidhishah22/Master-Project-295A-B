48cbcb8cc1f2c8919d287913a59243d2b2989092
“vidhishah22” <vidhirajesh.shah@sjsu.edu>
2019-11-21 17:33:43 -0800

    face detection face-api

---
+++ Face-Detector/script.js
@@ -0,0 +1,48 @@
+const video = document.getElementById('video')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/models'),
+]).then(startVideo)
+
+function startVideo(){
+    // navigator.getUserMedia(
+    //     {video: {}},
+    //     stream => video.srcObject = stream,
+    //     err => console.error(err)
+    // )
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+    })
+        .then(
+            (cameraStream) => {
+                video.srcObject = cameraStream;
+            }
+        )
+}
+
+video.addEventListener('play',() =>{
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width : video.width, height : video.height}
+    faceapi.matchDimensions(canvas, displaySize )
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, 
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        resizedDetections.forEach(detection => {
+            const box = detection.detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: "Age: " + Math.round(detection.age) + "\n Gender: " + detection.gender })
+            drawBox.draw(canvas)
+        })
+    }, 100)
+    
+})
