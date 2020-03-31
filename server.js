3856a5f847b94b2dcf75a5e423efbce66b0b2dca
Mojdeh Keykhanzadeh <mojdeh.keykhanzadeh@sjsu.edu>
2020-03-30 21:33:01 -0700

    added deployment file and some cleaning

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const app = express();
+const server = express();
 
 // View engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
 
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+server.use(cookieParser());
+server.use(express.static(path.join(__dirname, "public")));
+server.use(cors());
 
 // Allow access control, i.e., avoid CORS error
-app.use(function(req, res, next) {
+server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
@@ -30,11 +30,11 @@ app.use(function(req, res, next) {
 
 const homeRouter = require("./routes/home");
 
-app.use("/", homeRouter);
+server.use("/", homeRouter);
 
 const tryonRouter = require("./routes/tryon");
 
-app.use("/", tryonRouter);
+server.use("/", tryonRouter);
 
 // // https://expressjs.com/en/starter/basic-routing.html
 // app.get("/", (request, response) => {
@@ -42,9 +42,9 @@ app.use("/", tryonRouter);
 // });
 
 // send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
+server.get("/dreams", (request, response) => {
   // express helps us take JS objects and send them as JSON
   response.json(dreams);
 });
 
-module.exports = app;
+module.exports = server;
