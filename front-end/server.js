67f7750da8f579f90c2dd1ce570a43c9e64a40d6
vidhishah22 <vidhirajesh.shah@sjsu.edu>
2020-04-16 22:56:45 -0700

    clothes tryon-sidebyside camera view and results page

--- front-end/server.js
+++ front-end/server.js
@@ -20,7 +20,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 
 server.use(cookieParser());
-server.use(express.static(path.join(__dirname, "public")));
+server.use(express.static(path.join(__dirname, "/public")));
 server.use(cors());
 
 // Allow access control, i.e., avoid CORS error
